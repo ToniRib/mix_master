99fb0ece61de60b95c94d87407ae5cfdcd297c39
ToniRib <tonimarierib@gmail.com>
2015-12-21 15:03:09 -0700

    test: Proves songs for all artists are in index

--- spec/features/user_views_all_songs_spec.rb
+++ spec/features/user_views_all_songs_spec.rb
@@ -25,4 +25,22 @@ RSpec.feature "User views all songs" do
       expect(page).to have_content 'Feel Good Drag'
     end
   end
+
+  context "multiple artists with songs have been created" do
+    scenario "they see all songs regardless of artist" do
+      anberlin_song_titles = ['Alexithymia', 'Feel Good Drag', 'Dance, Dance, Christa Paffgen']
+      anberlin = create(:artist)
+      anberlin_songs = anberlin_song_titles.map { |title| anberlin.songs.create(title: title) }
+
+      incubus = Artist.create(name: 'Incubus', image_path: 'http://exampleimage.com')
+      incubus_song_titles = ['Warning', 'Sick Sad Little World']
+      incubus_songs = incubus_song_titles.map { |title| incubus.songs.create(title: title) }
+
+      visit songs_path
+
+      (anberlin_songs + incubus_songs).each do |song|
+        expect(page).to have_link song.title, href: song_path(song)
+      end
+    end
+  end
 end
