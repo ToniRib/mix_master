840ce1e9b647584b272f9e86a169baaab58932d9
ToniRib <tonimarierib@gmail.com>
2015-12-21 10:06:55 -0700

    feat: Adds index page for viewing links to each artist

---
+++ spec/features/user_views_all_artists_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature "User views all artists" do
+  scenario "they see a page with all artists" do
+    artists = ['Anberlin', 'Incubus', 'Young the Giant']
+
+    artists.map! do |name|
+      Artist.create(name: name, image_path: 'http://example.jpg')
+    end
+
+    visit artists_path
+
+    artists.each do |artist|
+      expect(page).to have_link artist.name, href: artist_path(artist)
+    end
+  end
+
+  context "there are no artists yet" do
+    scenario "they see a page saying no artists added" do
+
+    end
+  end
+end
