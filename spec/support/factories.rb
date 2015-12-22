449dce437333de71c0827b857b3371df067ca9e0
ToniRib <tonimarierib@gmail.com>
2015-12-22 09:26:07 -0700

    refactor: Updates factories and new song view

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,11 +1,29 @@
 FactoryGirl.define do
   factory :artist do
-    name       'Anberlin'
+    name
     image_path 'http://anberlin.com/bandlg.jpg'
+
+    factory :artist_with_songs do
+      transient do
+        songs_count 4
+      end
+
+      after(:create) do |artist, evaluator|
+        create_list(:song, evaluator.songs_count, artist: artist)
+      end
+    end
+  end
+
+  sequence :name do |n|
+    "Anberlin#{n}"
+  end
+
+  sequence :title, %w(A B C).cycle do |n|
+    "Title#{n}"
   end
 
   factory :song do
-    title "Feel Good Drag"
-    artist nil
+    title
+    artist
   end
 end
